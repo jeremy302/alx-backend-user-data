8821fe6de0778cabef54f1cddd9fe90e1e60964f
jeremy302 <olajeremy123@gmail.com>
2023-05-02 14:54:32 +0100

    added type check

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,7 +42,8 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
         ''' user object from credentials '''
-        if user_email is None or user_pwd is None:
+        if (user_email is None or type(user_email) is not str or
+            user_pwd is None or type(user_pwd) is not str):
             return None
         users = User.search({'email': user_email})
         if not users:
